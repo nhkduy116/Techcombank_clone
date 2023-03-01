510ee09d8a8700a16ac16cbbf9c9d1189b1f5365
duybn1106 <duynguyen11062002@gmail.com>
2023-03-01 14:40:36 +0700

    commit

--- techcombank_clone/lib/HomeLoginScreen.dart
+++ techcombank_clone/lib/HomeLoginScreen.dart
@@ -1,12 +1,17 @@
 // ignore_for_file: prefer_const_constructors, file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeLoginScreen extends StatelessWidget {
   const HomeLoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+            systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      ),
+    );
   }
 }
